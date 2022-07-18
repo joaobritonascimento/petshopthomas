cfcfc0754c738be7d4d92eaf6e70890bd0ca23bb
joaobritonascimento <joaobritonascimento@gmail.com>
2022-07-18 19:44:55 -0300

    #01 18072022

--- script.js
+++ script.js
@@ -15,21 +15,21 @@ const getStorage = () => {
 
 document.getElementById('lastname').onclick = getStorage;*/
 
-function salvarCadastro(){
-  if(localStorage.info){
-  localStorage.info = document.getElementById("inputcpf").value;
-  localStorage.info = document.getElementById("inputNome").value;
-  localStorage.info = document.getElementById("inputEndereco").value;
-  localStorage.info = document.getElementById("inputEmail").value;
-  localStorage.info = document.getElementById("inputNomePet").value;
-  localStorage.info = document.getElementById("inputRaca").value;
-  localStorage.info = document.getElementById("inputCidade").value;
-  localStorage.info = document.getElementById("inputEstado").value;
-  localStorage.info = document.getElementById("inputCEP").value;
+function salvarCadastro() {
+  if (localStorage.info) {
+    localStorage.info = document.getElementById("inputcpf").value;
+    localStorage.info = document.getElementById("inputNome").value;
+    localStorage.info = document.getElementById("inputEndereco").value;
+    localStorage.info = document.getElementById("inputEmail").value;
+    localStorage.info = document.getElementById("inputNomePet").value;
+    localStorage.info = document.getElementById("inputRaca").value;
+    localStorage.info = document.getElementById("inputCidade").value;
+    localStorage.info = document.getElementById("inputEstado").value;
+    localStorage.info = document.getElementById("inputCEP").value;
   }
 }
 
-function carregarCadastro(){
+function carregarCadastro() {
   document.getElementById("inputCPF").value = localStorage.info;
   document.getElementById("inputNome").value = localStorage.info;
   document.getElementById("inputNome").value = localStorage.info;
@@ -42,11 +42,29 @@ function carregarCadastro(){
   document.getElementById("inputCEP").value = localStorage.info;
 }
 
-function salvarCadastroteste(){
-  if(localStorage.info){
-  localStorage.info = document.getElementById("dado").value;
-  }}
-
-  function carregarCadastroteste(){
-    document.getElementById("dado").value = localStorage.info;
+function salvarCadastroteste() {
+  if (localStorage.info) {
+    localStorage.info = document.getElementById("dado").value;
   }
+}
+
+function carregarCadastroteste() {
+  document.getElementById("dado").value = localStorage.info;
+}
+
+/* <label>
+ <input id="dado"></label>
+<button type="button" onclick="salvarCadastroteste()">Salvar</button>
+<button type="button" onclick="carregarCadastroteste()">Carregar</button>
+
+<script>
+ function salvarCadastroteste(){
+   if (localStorage.info) {
+     localStorage.info = document.getElementById("dado").value;
+   }
+ }
+
+ function carregarCadastroteste(){
+   document.getElementById("dado").value = localStorage.info;
+ }
+</script>*/
